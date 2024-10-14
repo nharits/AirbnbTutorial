853668dd6c341f1b97b94ff5eb5232bed76c2e50
haritS <harits4dev@gmail.com>
2024-10-14 22:05:18 +0700

    Initial Commit

---
+++ AirbnbTutorial/AirbnbTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbTutorialApp.swift
+//  AirbnbTutorial
+//
+//  Created by haritS on 14/10/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

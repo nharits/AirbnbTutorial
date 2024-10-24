bd5d4c7884c365b1d732ff95620e14cae7d30838
haritS <harits4dev@gmail.com>
2024-10-24 13:04:47 +0700

    update MapView, add extension

--- AirbnbTutorial/Core/Listings/View/ListingDetailView.swift
+++ AirbnbTutorial/Core/Listings/View/ListingDetailView.swift
@@ -12,6 +12,18 @@ struct ListingDetailView: View {
     
     @Environment(\.dismiss) var dismiss
     let listing: Listing
+    @State private var cameraPosition : MapCameraPosition
+    
+    init(listing: Listing) {
+        self.listing = listing
+        
+        let region = MKCoordinateRegion(
+            center: listing.city == "Los Angeles" ? .losAngeles : .miami,
+            span: MKCoordinateSpan(latitudeDelta: 0.1,
+                                   longitudeDelta: 0.1))
+        
+        self._cameraPosition = State(initialValue: .region(region))
+    }
     
     var body: some View {
         ScrollView {
@@ -174,7 +186,7 @@ struct ListingDetailView: View {
                  Text("Where you will be")
                     .font(.headline)
             
-                 Map()
+                Map(position: $cameraPosition)
                     .frame(height: 200)
                     .clipShape(RoundedRectangle(cornerRadius: 12))
             }
